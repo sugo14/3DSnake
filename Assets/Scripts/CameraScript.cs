775bbc5be12b280b69e9ec41c92a5da883b87071
sugo14 <98863576+sugo14@users.noreply.github.com>
2025-01-14 10:21:53 -0700

    changes

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,24 +5,21 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     public GameObject snakeHead, cubePrefab;
-    public int minDist = 0, maxDist = 20;
-    public int cubeCount = 100, minSize = 1, maxSize = 5;
-    public float distance = 10, camSpeed = 20;
-    public bool firstPerson = false;
-    public float speed = 10;
-    Vector3 currPos;
+    public int minDist = 25, maxDist = 70;
+    public int cubeCount = 300, minSize = 1, maxSize = 9;
+    public float distance = 10, rotationSpeed = 2, moveSpeed = 2, foresight = 2;
 
     // Start is called before the first frame update
     void Start()
     {
-        currPos = new Vector3(0, 0, 0);
         for (int i = 0; i < cubeCount; i++) {
             GameObject instance = Instantiate(cubePrefab);
+            int x = Random.Range(0, 3);
             bool coinflip = Random.Range(0, 1) == 1;
             instance.transform.position = new Vector3(
-                (Random.Range(0, 2) == 1 ? 1 : -1) * Random.Range(minDist, maxDist),
-                (Random.Range(0, 2) == 1 ? 1 : -1) * Random.Range(minDist, maxDist),
-                (Random.Range(0, 2) == 1 ? 1 : -1) * Random.Range(minDist, maxDist)
+                x == 0 ? ((Random.Range(0, 2) == 1 ? 1 : -1) * Random.Range(minDist, maxDist + 1)) : Random.Range(-maxDist, maxDist + 1),
+                x == 1 ? ((Random.Range(0, 2) == 1 ? 1 : -1) * Random.Range(minDist, maxDist + 1)) : Random.Range(-maxDist, maxDist + 1),
+                x == 2 ? ((Random.Range(0, 2) == 1 ? 1 : -1) * Random.Range(minDist, maxDist + 1)) : Random.Range(-maxDist, maxDist + 1)
             );
             int scale = Random.Range(minSize, maxSize);
             instance.transform.localScale = new Vector3(scale, scale, scale);
@@ -41,9 +38,15 @@ public class CameraScript : MonoBehaviour
 
     void Update() {
         Transform t = gameObject.transform;
-        Vector3 desiredPos = snakeHead.transform.position + Vector3.ClampMagnitude(snakeHead.transform.position, 1) * 10;
+        CubeOrient cubeOrient = snakeHead.GetComponent<SnakeHeadScript>().orient;
+        for (int i = 0; i < foresight; i++) {
+            cubeOrient = new CubeOrient(cubeOrient);
+            cubeOrient.Go();
+        }
+        Vector3 futurePos = cubeOrient.WorldPosition();
+        Vector3 desiredPos = futurePos + Vector3.ClampMagnitude(futurePos, 1) * distance;
         Quaternion desiredRot = Quaternion.LookRotation(Vector3.zero - desiredPos, snakeHead.GetComponent<SnakeHeadScript>().orient.SnakeUp());
-        transform.position = Vector3.Slerp(transform.position, desiredPos, speed * Time.deltaTime);
-        transform.rotation = Quaternion.Slerp(transform.rotation, desiredRot, speed * Time.deltaTime);
+        transform.position = Vector3.Slerp(transform.position, desiredPos, moveSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, desiredRot, rotationSpeed * Time.deltaTime);
     }
 }
